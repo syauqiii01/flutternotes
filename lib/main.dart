81732a36348cf09c2679e7332cbd8de8610e6173
S Y A U Q I <revosyauqi1@gmail.com>
2025-03-04 22:13:37 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutternotes/firebase_options.dart';
+import 'package:flutternotes/views/loginview.dart';
+import 'package:flutternotes/views/registerview.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RegisterView(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('You Are a Verified User');
+              } else {
+                print('You Need To Verify Your Email First');
+              }
+              return const Text('Done');
+            default:
+              return const Text('Loading... ');
+          }
+        },
+      ),
+    );
+  }
+}
